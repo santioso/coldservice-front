9154ff8b59df598a0862c751b0c67e907ff651d4
santioso <santia.osorio@gmail.com>
2026-07-07 12:16:11 -0500

    fix: update monitoring JWT interceptor

--- src/app/core/interceptor/jwt.interceptor.ts
+++ src/app/core/interceptor/jwt.interceptor.ts
@@ -1,24 +1,45 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-} from "@angular/common/http";
-import { Observable } from "rxjs";
-import { AuthService } from "../service/auth.service";
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../service/auth.service';
+import { MonitoringAuthService } from 'app/monitoring/monitoring-auth.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  constructor(private authenticationService: AuthService) {}
+  constructor(
+    private readonly authenticationService: AuthService,
+    private readonly monitoringAuthService: MonitoringAuthService,
+  ) {}
 
   intercept(
     request: HttpRequest<any>,
-    next: HttpHandler
+    next: HttpHandler,
   ): Observable<HttpEvent<any>> {
+    // No añadir encabezado de autorización para solicitudes a ThingSpeak
+    if (request.url.includes('api.thingspeak.com')) {
+      return next.handle(request);
+    }
+
+    if (request.url.includes('/api/v1/monitoring/')) {
+      const monitoringToken = this.monitoringAuthService.token;
+      if (monitoringToken) {
+        request = request.clone({
+          setHeaders: {
+            Authorization: `Bearer ${monitoringToken}`,
+          },
+        });
+      }
+      return next.handle(request);
+    }
+
     // add authorization header with jwt token if available
     const currentUser = this.authenticationService.currentUserValue;
-    if (currentUser && currentUser.access_token) {
+    if (currentUser?.access_token) {
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${currentUser.access_token}`,
